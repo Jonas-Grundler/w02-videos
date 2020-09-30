92d1bef80c97a2059b1933cbb7af756d0511b10c
Jonas Grundler <s2065226@ed.ac.uk>
2020-09-30 14:25:44 +0200

    Practice Quiz 1 completed

--- playground.py
+++ playground.py
@@ -72,3 +72,50 @@ def floats(x,y,z):
     print(str(a)+', '+str(b)+', '+str(c))
 
 floats(3.2,7.0,2.4)
+
+'Ex 9'
+def miles_to_meters():
+    speed_miles = input("Please input your speed in miles per hour: ")
+    x = int(speed_miles)
+    speed_new = round(x * (1609.34/3600),2)
+    print(str(x)+" mph = "+str(speed_new)+" m/s.")
+
+miles_to_meters()
+
+'Ex 10'
+import math
+
+def infinite_series():
+    a = 0
+    for i in range(1,201):
+        a = a + (1/(i**2))
+
+    e = (math.pi**2)/6
+
+    a = round(a,4)
+    e = round(e,4)
+    diff = round(e-a,4)
+
+    print("The exact value of this infinite series is "+str(e)+".")
+    print("The first 200 terms in the series give a total of "+str(a)+".")
+    print("The difference between these two values is "+str(diff)+".")
+
+infinite_series()
+
+
+'Ex 11'
+def string_rev_ext(x):
+    return(x+":"+x[::-1])
+
+print(string_rev_ext("abc"))
+
+'Ex 12'
+def geometric():
+    res = 0
+    a = 3
+    r = 2
+    for m in range(0,26):
+        res = res + a*(r**m)
+    print(res)
+
+geometric()
